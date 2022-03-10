8a5cc342bf920d03a8a977fbdb3eeb9880c53388
Rsoline <rsolinemartinho@yahoo.com>
2022-03-10 18:22:15 +0000

    Remove the variable; score, about text, instruction

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 let question = "";
 let choices = "";
 let answer = true;
+let score = 0;
 
 // define all elements
 const startGameBtn = document.getElementById("start-reset");
-const instructionSection = document.getElementById("instructionSection");
-const questionHolder = documentById("question");
+const instructionSession = document.getElementById("instructionSection");
+const gamesection = documentById("game-section");
 let boxes = document.getElementById("box");
 
+// event handles
+function startGame() {
+    score = 0;
+    instructionSession.getElementsByClassName.display = 'none'
+    startGame.style.display = 'none'
+    gamesection.style.display = 'block';
+}
