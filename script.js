ad1d4e62b325dcea1635f80c5f47e2c6ecb69fed
Rsoline <rsolinemartinho@yahoo.com>
2022-03-10 17:26:59 +0000

    Set variables and define all elements

--- script.js
+++ script.js
@@ -1,38 +1,10 @@
-var question = "";
-var choices = "";
-var answer = true;
+let question = "";
+let choices = "";
+let answer = true;
 
-var questionBank= [
-    {
-      question : 'What is the capital of Ucraine?';
-      option : ['Warsaw','Tallin','Kiev','Budapest'],
-      answer : 'Kiev'
-    },
-    {
-      question : 'What is the capital of Poland?';
-      option : ['Warsaw','Wroclaw','Budapest','Berlin'],
-      answer : 'Warsaw'
-    }
-    {
-      questin : 'What is the capital of United Kingdom?';
-      option : ['Dublin','London','Menchester','Leeds'],
-      answer : 'London'
-    }
-    {
-      question : 'What is the capoital of Germany?';
-      option : ['Berlin','Warsaw','Helsinki','Luksemburg'],
-      answer : 'Berlin'
-    }
-    {
-      question : 'What is the capoital of Vatican?'
-      option : ['Roma','Vatica City','Nice','Monaco'],
-      answer : 'Vatican City'
-    }
-    {
-      {
-        question : 'What is the capoital of Romania?'
-        option : ['Budapest','Tallin','Riga','Bucharest'],
-        answer : 'Bucharest'
-      }
-    }
-  ]
+// define all elements
+const startGameBtn = document.getElementById("start-reset");
+const instructionSection = document.getElementById("instructionSection");
+const questionHolder = documentById("question");
+let boxes = document.getElementById("box");
+
