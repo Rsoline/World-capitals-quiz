02408e084cbe66f0542802dca87584dcd6c4093c
Rsoline <rsolinemartinho@yahoo.com>
2022-03-10 18:46:05 +0000

    Add questions to questionBank

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
-let question = "";
+let questionIndex = 0;
 let choices = "";
 let answer = true;
 let score = 0;
+
+const questionBank = [
+{
+    text : 'What is the capitol of Ucraine?'
+    option : ['Warsaw', 'Tallinn', 'Kiev', 'Budapest'],
+    answer : 'Kiev'
+},
+{
+    text : 'What is the capitol of Poland?'
+    option : ['Warsaw', 'Wroclaw', 'Budapest', 'Berlin'],
+    answer : 'Warsaw'
+},
+{
+    text : 'What is the capitol of United Kingdom?'
+    option : ['Dublin', 'London', 'Manchester', 'Liverpool'],
+    answer : 'London'
+},
+{
+    text : 'What is the capitol of Germany?'
+    option : ['Berlin', 'Warswa', 'Helsinki', 'Luksemburg'],
+    answer : 'Berlin'
+},
+{
+    text : 'What is the capitol of V`tican?'
+    option : ['Roma', 'Vatican City', 'Nice', 'Monaco'],
+    answer : 'Vatican city'
+},
+{
+    text : 'What is the capitol of Romania?'
+    option : ['Budapest', 'Tallinn', 'Kiev', 'Bucharest'],
+    answer : 'Kiev'
+},
+
+]
 
 // define all elements
 const startGameBtn = document.getElementById("start-reset");
@@ -12,7 +46,13 @@ let boxes = document.getElementById("box");
 // event handles
 function startGame() {
     score = 0;
+    questionIndex = 0;
     instructionSession.getElementsByClassName.display = 'none'
-    startGame.style.display = 'none'
+    startGameBtn.style.display = 'none'
     gamesection.style.display = 'block';
+    showQuestion(questionBank[questionIndex]);
+}
+
+function showQuestion(question) {
+
 }
